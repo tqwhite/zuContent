f60443bdd47660e261084ea089defb292ba287a1
TQ White II <tq@justkidding.com>
2014-04-19 21:54:20 -0500

    nav senses current page, starting to build front page

--- main/_GLOBAL/JS/scrollPanel.js
+++ main/_GLOBAL/JS/scrollPanel.js
@@ -3,6 +3,80 @@ $('.menuActivateButton').click(function(item){
 	$('.menuButtonResponder').toggle();
 });
 
+var browserSpecs=Widgets.Models.Browser.windowSize(),
+	tabList=$('#tabList');
+$('.switchablePanel').each(
+
+	function(inx, item){
+	 		$(item).css({
+ 			height:browserSpecs.height+'px',
+ 			width:browserSpecs.width+'px'
+ 		});
+ 
+ 		$('.fullScreenJs', item).css({
+ 			height:browserSpecs.height+'px',
+ 			width:browserSpecs.width+'px'
+ 		});
+	if (inx===0){
+	
+		var frontTabList=tabList.clone(),
+			panelId=$(item).attr('id');
+		frontTabList.addClass('tabList').addClass('navFrontPanel');
+		$(item).append(frontTabList);
+
+		frontTabList.attr('id', 'frontPageTablist').show();
+	}
+	else{
+		var newTabList=tabList.clone();
+		newTabList.addClass('tabList').addClass('menuButtonResponder').addClass('navSubsequentPanels');
+		$(item).prepend(newTabList);
+	a=1;
+	}
+
+		
+	}
+
+);
+
+
+$('.panelTab a').click(function(event){
+event.preventDefault();
+
+//	$('.menuButtonResponder').hide();
+
+	var currPanelId=$(this).parent().parent().parent().attr('id');
+	var targetSelector=this.href.match(/.*(#.*)$/)[1],
+		target=$(targetSelector);			
+		var titleString=this.text
+		origTitle=document.title;
+		
+		if (currPanelId!=targetSelector){
+		
+
+		
+		$.scrollTo(target, 2000, {
+			onAfter: function() {		
+				if(titleString){document.title=origTitle+': '+titleString;}
+				location.hash=targetSelector;
+				var linkId='linkto_'+targetSelector.replace(/#/, '');
+				
+				
+	
+// 	tabList.find('a').removeClass('navActive');	
+// 	$('.masterPanelList').find('#'+'linkId').parent().parent().removeClass('navActive').addClass('navInactive');
+	
+	$('.masterPanelList').find('.panelTab').removeClass('navInactive').addClass('navActive');	
+	$('.masterPanelList').find('#'+linkId).parent().parent().removeClass('navActive').addClass('navInactive');
+				
+			}
+			
+			});
+
+			
+
+		}
+});
+
 
 //	$('#tabList').addClass('navFrontPanel').show();
 	
@@ -29,68 +103,3 @@ $('.menuActivateButton').click(function(item){
 // 	};
 
 //$('.switchablePanel').bind('click', scrollFunction)
-
-var browserSpecs=Widgets.Models.Browser.windowSize(),
-	tabList=$('#tabList');
-$('.switchablePanel').each(
-
-	function(inx, item){
-	 		$(item).css({
- 			height:browserSpecs.height+'px',
- 			width:browserSpecs.width+'px'
- 		});
- 
- 		$('img', item).css({
- 			height:browserSpecs.height+'px',
- 			width:browserSpecs.width+'px'
- 		});
-	if (inx===0){
-	
-		var frontTabList=tabList.clone(),
-			panelId=$(item).attr('id');
-		frontTabList.addClass('tabList').addClass('navFrontPanel');
-		$(item).append(frontTabList);
-
-		frontTabList.attr('id', 'frontPageTablist').show();
-	}
-	else{
-		var newTabList=tabList.clone();
-		newTabList.addClass('tabList').addClass('menuButtonResponder').addClass('navSubsequentPanels');
-		$(item).prepend(newTabList);
-	a=1;
-	}
-
-
-		
-	}
-
-);
-
-
-
-
-$('.panelTab a').click(function(event){
-event.preventDefault();
-	$('.tabList').hide();
-	var currPanelId=$(this).parent().parent().parent().attr('id');
-	var targetSelector=this.href.match(/.*(#.*)$/)[1],
-		target=$(targetSelector);			
-		var titleString=this.text
-		origTitle=document.title;
-		
-		if (currPanelId!=targetSelector){
-		
-
-		
-		$.scrollTo(target, 2000, {
-			onAfter: function() {		
-				if(titleString){document.title=origTitle+': '+titleString;}
-				location.hash=targetSelector;
-			}
-			
-			});
-
-			
-
-		}
-});
